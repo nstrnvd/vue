11b4587553550b3b82f21b169e31ff01926f86d8
nastaran valizade <nastaranvalizade2@gmail.com>
2023-07-26 18:17:50 +0330

    fixed problem

--- main.js
+++ main.js
@@ -8,11 +8,37 @@ document.addEventListener("DOMContentLoaded", function () {
       Link: {
         href: 'https://v2.vuejs.org/v2/guide/index.html'
       },
+      imageSource: "imges/green.jpg",
+      imageAlt: "green-socks",
+      variants: [
+        {
+          variantId: 2233,
+          variantColor: "green",
+          variantImage: "imges/green.jpg",
+        },
+        {
+          variantId: 2234,
+          variantColor: "blue",
+          variantImage: "imges/blue.png",
+        },
+      ],
       sizes: [{ key: 0, size: 'small' }, { key: 1, size: 'Medium' }, { key: 2, size: 'Large' }],
       Title: {
         text: 'This is a Title'
       },
-      inSale: true
+      inSale: true,
+      cart: 0,
+    },
+    methods: {
+      addToCart: function () {
+        this.cart += 1
+      },
+      removeFromToCart: function () {
+        this.cart -= 1
+      },
+      updatePtoduct: function(variantImage) {
+        this.imageSource = variantImage;
+      },
     }
   })
   Vue.config.devtools = true;
